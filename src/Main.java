c1fd35bdf8a54dec11684bc1346d0f7c103625f5
Roman Leshin <vorzee@gmail.com>
2024-08-14 16:19:22 +0500

    Поправили неточность в тестовых данных

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double heightInMeters = 170;
-        double weightInKilograms = 70;
+        double heightInMeters = 1.87;
+        double weightInKilograms = 98;
         int index = service.calculate(heightInMeters, weightInKilograms); // должно получиться 28
         System.out.println(index);
     }
